fcbd48eb91edabd69423f1b9573910a7b8622e5a
R. Biswas <rbiswas4@gmail.com>
2016-09-17 12:24:34 -0700

    setting path at every stage

--- continuous_integration/setup_env.sh
+++ continuous_integration/setup_env.sh
@@ -5,3 +5,5 @@ export CACHE_DIR="${store_dir}/miniconda.tarball"
 export CACHE_DIR_TMP="$CACHE_DIR.tmp"
 export CACHE_TARBALL_NAME="miniconda.tar.gz"
 export CACHE_TARBALL_PATH="$CACHE_DIR/$CACHE_TARBALL_NAME"
+export PATH="${store_dir}/miniconda/bin:$PATH"
+
