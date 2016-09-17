11bb9863c9eef057e7c8b6f52a25f372ce2e8c22
R. Biswas <rbiswas4@gmail.com>
2016-09-16 21:52:21 -0700

    removing env

--- continuous_integration/conda_install_dep.sh
+++ continuous_integration/conda_install_dep.sh
@@ -7,8 +7,8 @@ CHANNEL=${CHANNEL:-"pandas"}
 #conda config --add channels "$CHANNEL"
 CHANNEL=${CHANNEL:-"astropy"}
 conda config --add channels "$CHANNEL"
-conda create --yes -n AnalyzeSN python
-source activate AnalyzeSN
+# conda create --yes -n AnalyzeSN python
+# source activate AnalyzeSN
 conda install -q --yes --file ./continuous_integration/requirements.txt
 conda list --explicit > spec-file.txtconda list --explicit > spec-file.txt;
 
